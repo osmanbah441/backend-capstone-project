a74c4ed5891b80a5847206683e26dd891823e5be
osman bah <82353262+osmanbah441@users.noreply.github.com>
2024-04-27 13:37:22 +0000

    Update urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu-items/', views.MenuItemView.as_view()),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('login/', obtain_auth_token)
+    path('api-token-auth/', obtain_auth_token)
 ]
