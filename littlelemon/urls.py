9ce5244922e185f67f881c9c8cea67559d445f07
Osman Bah <osmanbah441@gmail.com>
2024-04-27 12:00:33 +0000

    added testing for model and views

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -24,7 +24,7 @@ router.register(r'tables',views.BookingViewSet, 'tables' )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls')),
+    path('api/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
